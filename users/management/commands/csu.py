fdfc76bccb42732311180d127f5666fc585696ed
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-11-19 20:42:33 +0300

    added csu command

---
+++ users/management/commands/csu.py
@@ -0,0 +1,20 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    """
+    Создает супер-пользователя при вызове комманды
+    """
+
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email='admin2@mail.ru',
+            is_superuser=True,
+            is_staff=True,
+            is_active=True
+        )
+
+        user.set_password('admin2')
+        user.save()
