6a0c1991945bea92df44aae12ffda0a1a3040354
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-11-19 20:41:06 +0300

    created user model

--- users/models.py
+++ users/models.py
@@ -1,17 +1,52 @@
+from django.apps import apps
+from django.contrib.auth.base_user import BaseUserManager
+
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 NULLABLE = {'null': True, 'blank': True}
 # Create your models here.
+class UserManager(BaseUserManager):
+    """
+    Модель упрвления пользователями
+    """
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The given username must be set")
+        email = self.normalize_email(email)
+        GlobalUserModel = apps.get_model(
+            self.model._meta.app_label, self.model._meta.object_name
+        )
+        email = GlobalUserModel.normalize_username(email)
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+
 class User(AbstractUser):
+    """
+    Модель пользователя
+    """
     username = None
 
     email = models.EmailField(unique=True, verbose_name='почта')
     phone = models.CharField(max_length=20, verbose_name='телефон', **NULLABLE)
-    city = models.CharField(max_length=50, verbose_name='город', **NULLABLE)
+    telegram_nickname = models.CharField(max_length=100, verbose_name='никнейм в тг', **NULLABLE)
+    chat_id = models.BigIntegerField(verbose_name='id чата бота', **NULLABLE)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     def __str__(self):
         return f'Пользователь {self.email}'
