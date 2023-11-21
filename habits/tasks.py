1a88d31e9a67948452d75bd09902cd2bbca77a53
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-11-21 15:23:09 +0300

    small changes

--- habits/tasks.py
+++ habits/tasks.py
@@ -14,10 +14,10 @@ def send_habit_tg_task(pk):
     chat_id = habit.owner.chat_id
     related_habit = habit.related_habit
 
-    if habit.related_habit:
-        text=f'Напоминание о привычке!' '\n' f'{habit.__str__()}' '\n' f'А после {related_habit.__str__}'
+    if related_habit:
+        text=f'Напоминание о привычке!' '\n' f'{habit.__str__()}' '\n' f'А после {related_habit.__str__()}'
     else:
-        text=f'Напоминание о привычке!' '\n' f'{habit.__str__()}' '\n' f'И получу {habit.reward}'
+        text=f'Напоминание о привычке!' '\n' f'{habit.__str__()}'
 
     requests.post(f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={text}")
 
